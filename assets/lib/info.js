feaf1d891a9dc8758b3939bb75b6e58d1837957b
Oddeon <77466077+Oddeon@users.noreply.github.com>
2021-01-14 18:58:57 -0300

    Add files via upload

---
+++ assets/lib/info.js
@@ -0,0 +1,43 @@
+exports.info = (id, A187, tampilTanggal, whatsapp, youtube, tampilWaktu, instagram, nomer, aktif) => {
+	return ` 
+❉──────────❉
+
+*${A187}*
+
+❉──────────❉
+
+📆 *${tampilTanggal}*
+Status do Bot ON: *${aktif}*
+
+         
+╔════════════════════
+║ *Feito por: $Dark*
+╠════════════════════
+║   *⚠️ATENÇÃO⚠️*
+╠════════════════════
+║*OS CRÉDITOS NAO SÃO MEUS*
+║*APENAS FIZ UMA VERSÃO INSTÁVEL*
+╠════════════════════
+║╭──❉ *INFO* ❉──
+║│➸ *AUTOR*: Higor
+║│➸ *DESIGNER*: Higor
+║│➸ *YOUTUBE*: Dark ツ
+║╰───────────
+╠════════════════════
+║╭──❉ *SOSMED ADMIN* ❉──
+║│ _${whatsapp}_
+║│➸ *YouTube*
+║│ _${nomer.}_
+║│➸ *Instagram <Segue>*
+║│ _${instagram}_
+║│➸ *Criador*
+║│    Dark ツ
+║╰───────────
+║         ${A187}
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║         ${A187}
+╠════════════════════
+║ _*Feito por Dark ツ*_
+╚════════════════════`
+}
